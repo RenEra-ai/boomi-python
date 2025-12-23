9a7c7fad882c54b29ef09086ea982f06b6e99654
Glebuar <glebuar@gmail.com>
2025-12-22 19:43:34 -0800

    fix AS2 models: make required fields optional for API compatibility

--- src/boomi/models/as2_my_company_info.py
+++ src/boomi/models/as2_my_company_info.py
@@ -32,36 +32,40 @@ class As2MyCompanyInfo(BaseModel):
 
     def __init__(
         self,
-        as2_id: str,
-        encryption_private_certificate: PrivateCertificate,
-        mdn_signature_private_certificate: PrivateCertificate,
-        signing_private_certificate: PrivateCertificate,
+        as2_id: str = SENTINEL,
+        encryption_private_certificate: PrivateCertificate = SENTINEL,
+        mdn_signature_private_certificate: PrivateCertificate = SENTINEL,
+        signing_private_certificate: PrivateCertificate = SENTINEL,
         enabled_legacy_smime: bool = SENTINEL,
         **kwargs,
     ):
         """As2MyCompanyInfo
 
-        :param as2_id: as2_id
-        :type as2_id: str
+        :param as2_id: as2_id, defaults to None
+        :type as2_id: str, optional
         :param enabled_legacy_smime: enabled_legacy_smime, defaults to None
         :type enabled_legacy_smime: bool, optional
-        :param encryption_private_certificate: encryption_private_certificate
-        :type encryption_private_certificate: PrivateCertificate
-        :param mdn_signature_private_certificate: mdn_signature_private_certificate
-        :type mdn_signature_private_certificate: PrivateCertificate
-        :param signing_private_certificate: signing_private_certificate
-        :type signing_private_certificate: PrivateCertificate
+        :param encryption_private_certificate: encryption_private_certificate, defaults to None
+        :type encryption_private_certificate: PrivateCertificate, optional
+        :param mdn_signature_private_certificate: mdn_signature_private_certificate, defaults to None
+        :type mdn_signature_private_certificate: PrivateCertificate, optional
+        :param signing_private_certificate: signing_private_certificate, defaults to None
+        :type signing_private_certificate: PrivateCertificate, optional
         """
-        self.as2_id = as2_id
+        if as2_id is not SENTINEL:
+            self.as2_id = as2_id
         if enabled_legacy_smime is not SENTINEL:
             self.enabled_legacy_smime = enabled_legacy_smime
-        self.encryption_private_certificate = self._define_object(
-            encryption_private_certificate, PrivateCertificate
-        )
-        self.mdn_signature_private_certificate = self._define_object(
-            mdn_signature_private_certificate, PrivateCertificate
-        )
-        self.signing_private_certificate = self._define_object(
-            signing_private_certificate, PrivateCertificate
-        )
+        if encryption_private_certificate is not SENTINEL:
+            self.encryption_private_certificate = self._define_object(
+                encryption_private_certificate, PrivateCertificate
+            )
+        if mdn_signature_private_certificate is not SENTINEL:
+            self.mdn_signature_private_certificate = self._define_object(
+                mdn_signature_private_certificate, PrivateCertificate
+            )
+        if signing_private_certificate is not SENTINEL:
+            self.signing_private_certificate = self._define_object(
+                signing_private_certificate, PrivateCertificate
+            )
         self._kwargs = kwargs
--- src/boomi/models/as2_receive_options.py
+++ src/boomi/models/as2_receive_options.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 from .utils.json_map import JsonMap
 from .utils.base_model import BaseModel
+from .utils.sentinel import SENTINEL
 from .as2_partner_info import As2PartnerInfo
 from .as2_mdn_options import As2MdnOptions
 from .as2_message_options import As2MessageOptions
@@ -19,45 +20,49 @@ from .as2_my_company_info import As2MyCompanyInfo
 class As2ReceiveOptions(BaseModel):
     """As2ReceiveOptions
 
-    :param as2_default_partner_info: as2_default_partner_info
-    :type as2_default_partner_info: As2PartnerInfo
-    :param as2_default_partner_mdn_options: as2_default_partner_mdn_options
-    :type as2_default_partner_mdn_options: As2MdnOptions
-    :param as2_default_partner_message_options: as2_default_partner_message_options
-    :type as2_default_partner_message_options: As2MessageOptions
-    :param as2_my_company_info: as2_my_company_info
-    :type as2_my_company_info: As2MyCompanyInfo
+    :param as2_default_partner_info: as2_default_partner_info, defaults to None
+    :type as2_default_partner_info: As2PartnerInfo, optional
+    :param as2_default_partner_mdn_options: as2_default_partner_mdn_options, defaults to None
+    :type as2_default_partner_mdn_options: As2MdnOptions, optional
+    :param as2_default_partner_message_options: as2_default_partner_message_options, defaults to None
+    :type as2_default_partner_message_options: As2MessageOptions, optional
+    :param as2_my_company_info: as2_my_company_info, defaults to None
+    :type as2_my_company_info: As2MyCompanyInfo, optional
     """
 
     def __init__(
         self,
-        as2_default_partner_info: As2PartnerInfo,
-        as2_default_partner_mdn_options: As2MdnOptions,
-        as2_default_partner_message_options: As2MessageOptions,
-        as2_my_company_info: As2MyCompanyInfo,
+        as2_default_partner_info: As2PartnerInfo = SENTINEL,
+        as2_default_partner_mdn_options: As2MdnOptions = SENTINEL,
+        as2_default_partner_message_options: As2MessageOptions = SENTINEL,
+        as2_my_company_info: As2MyCompanyInfo = SENTINEL,
         **kwargs,
     ):
         """As2ReceiveOptions
 
-        :param as2_default_partner_info: as2_default_partner_info
-        :type as2_default_partner_info: As2PartnerInfo
-        :param as2_default_partner_mdn_options: as2_default_partner_mdn_options
-        :type as2_default_partner_mdn_options: As2MdnOptions
-        :param as2_default_partner_message_options: as2_default_partner_message_options
-        :type as2_default_partner_message_options: As2MessageOptions
-        :param as2_my_company_info: as2_my_company_info
-        :type as2_my_company_info: As2MyCompanyInfo
+        :param as2_default_partner_info: as2_default_partner_info, defaults to None
+        :type as2_default_partner_info: As2PartnerInfo, optional
+        :param as2_default_partner_mdn_options: as2_default_partner_mdn_options, defaults to None
+        :type as2_default_partner_mdn_options: As2MdnOptions, optional
+        :param as2_default_partner_message_options: as2_default_partner_message_options, defaults to None
+        :type as2_default_partner_message_options: As2MessageOptions, optional
+        :param as2_my_company_info: as2_my_company_info, defaults to None
+        :type as2_my_company_info: As2MyCompanyInfo, optional
         """
-        self.as2_default_partner_info = self._define_object(
-            as2_default_partner_info, As2PartnerInfo
-        )
-        self.as2_default_partner_mdn_options = self._define_object(
-            as2_default_partner_mdn_options, As2MdnOptions
-        )
-        self.as2_default_partner_message_options = self._define_object(
-            as2_default_partner_message_options, As2MessageOptions
-        )
-        self.as2_my_company_info = self._define_object(
-            as2_my_company_info, As2MyCompanyInfo
-        )
+        if as2_default_partner_info is not SENTINEL:
+            self.as2_default_partner_info = self._define_object(
+                as2_default_partner_info, As2PartnerInfo
+            )
+        if as2_default_partner_mdn_options is not SENTINEL:
+            self.as2_default_partner_mdn_options = self._define_object(
+                as2_default_partner_mdn_options, As2MdnOptions
+            )
+        if as2_default_partner_message_options is not SENTINEL:
+            self.as2_default_partner_message_options = self._define_object(
+                as2_default_partner_message_options, As2MessageOptions
+            )
+        if as2_my_company_info is not SENTINEL:
+            self.as2_my_company_info = self._define_object(
+                as2_my_company_info, As2MyCompanyInfo
+            )
         self._kwargs = kwargs
